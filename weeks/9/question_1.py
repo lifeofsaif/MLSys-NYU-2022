8f4056afe0db99f308c097e27902ac1141be800f
abdur rahman <saif945383@gmail.com>
2025-04-20 02:47:20 +0000

    homework 4 improvements

--- weeks/9/question_1.py
+++ weeks/9/question_1.py
@@ -1,222 +1,143 @@
-'''
-# Homework #4 (due 11/04/2022)
-
-## Task 1: Experiment tracking (with Comet) and hyperparameter tuning (75%)
-Thanks to our partnership with Comet ML, we can use their experiment tracking tool for free! We discussed the importance of experiment tracking in the class, and in this assignment you will be asked to start doing it! Consider again [small_flow.py](https://github.com/jacopotagliabue/MLSys-NYU-2022/blob/main/weeks/8/src/small_flow.py)  (the simple DAG-based regression training).
-
- Duplicate the script, change the class name, and change the `train_model` step: instead of a linear regression, use any other scikit-available algorithm from the class, as long as the algorithm has one hyperparameter (make sure to use a dataset that makes sense with the algorithm you choose - if you choose a classification algorithm, you need to generate an appropriate synthetic dataset). 
- 
- Instead of training one model, you will:
-
-* Make sure to have a validation split, not just train and test split.
-* Use metaflow parallelization capabilities (foreach or branch!) to train multiple models: pick 4 values for the hyperparameter and train the model 4 times, each time with a different value.
-* Evaluate the models on the validation set and pick the best one.
-* Make sure to log the experiments in Comet: does the dashboard clearly show what model is the best?
-
-When you submit the code, submit both the new script and screenshot / prints from your Comet dashboard, clearly illustrating the tracking (you can also share the dashboard with your TA)
-
-Useful links: [Comet scikit docs](https://www.comet.com/docs/v2/integrations/ml-frameworks/scikit-learn/)
-, [Metaflow foreach docs](https://docs.metaflow.org/metaflow/basics).
-
-'''
-
 from metaflow import FlowSpec, step, Parameter, IncludeFile, current
 from datetime import datetime
 import os
 import numpy as np
 import pandas as pd
-from comet_ml import ExistingExperiment, Experiment
-from sklearn.preprocessing import StandardScaler
+from comet_ml import Experiment
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.utils.multiclass import type_of_target
+import uuid
+from io import StringIO
 
-assert os.environ.get('METAFLOW_DEFAULT_DATASTORE', 'local') == 'local'
-assert os.environ.get('METAFLOW_DEFAULT_ENVIRONMENT', 'local') == 'local'
-
-assert 'COMET_API_KEY' in os.environ and os.environ['COMET_API_KEY']
-assert 'MY_PROJECT_NAME' in os.environ and os.environ['MY_PROJECT_NAME']
+COLUMN_NAMES = [
+    "age", "workclass", "fnlwgt", "education", "education_num",
+    "marital_status", "occupation", "relationship", "race", "sex",
+    "capital_gain", "capital_loss", "hours_per_week", "native_country", "income"
+]
+TARGET_VALUE = "occupation"
+PARAMETER_GRID = [
+    {"C": 0.1, "penalty": "l2", "solver": "lbfgs", "fit_intercept": True},
+    {"C": 1.0, "penalty": "l2", "solver": "lbfgs", "fit_intercept": True},
+    {"C": 10.0, "penalty": "l2", "solver": "lbfgs", "fit_intercept": False},
+    {"C": 1.0, "penalty": "l2", "solver": "newton-cg", "fit_intercept": True},
+    {"C": 1e-10, "penalty": "l2", "solver": "newton-cg", "fit_intercept": True},
+]
 
 class LogisticRegressionFlow(FlowSpec):
-    """
-    LogisticRegressionFlow is a minimal DAG showcasing reading data from a file,
-    cleaning and prepping data, feature engineering, and then 
-    """
 
-    DATA_FILE = IncludeFile(
-        'dataset',
-        help='Census data from https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data',
-        is_text=True,
-        default='data/data.csv'
-    )
+    DATA_FILE = IncludeFile('dataset', help='Census data', is_text=True, default='data/data.csv')
+    TEST_SPLIT = Parameter('test_split', help='Proportion for test split', default=0.12)
 
-    TEST_SPLIT = Parameter(
-        name='test_split',
-        help='Determining the split of the dataset for testing',
-        default=0.20
-    )
-    
     @step
     def start(self):
-      """
-      Start up and print out some info to make sure everything is ok metaflow-side
-      """
-      print("Starting up at {}".format(datetime.utcnow()))
-      # debug printing - this is from https://docs.metaflow.org/metaflow/tagging
-      # to show how information about the current run can be accessed programmatically
-      print("flow name: %s" % current.flow_name)
-      print("run id: %s" % current.run_id)
-      print("username: %s" % current.username)
-      self.next(self.load_data)
-      
-      
-    @step
-    def load_data(self): 
-      """
-      Read the data in from the static file
-      """
-      from io import StringIO
-      
-      df = pd.read_csv(StringIO(self.DATA_FILE), header=None)
-      self.df = df
-      self.next(self.prepare_data)
-      
-    @step 
-    def prepare_data(self):
-      """
-      Prepare data. May be best to split this up later
-      - adds column names
-      - replaces ? with nan
-      - one hot encodes 
-      - drops any unused columns
-      - 
-      """
-      print("Preparing data")
-      df = self.df
-      
-      # add columns since columns are missing
-      column_names = [
-        "age", "workclass", "fnlwgt", "education", "education_num",
-        "marital_status", "occupation", "relationship", "race", "sex",
-        "capital_gain", "capital_loss", "hours_per_week", "native_country", "income"
-      ]
-      df.columns = column_names
-      
-      # this dataset has a ? for missing values, so replace it with nan
-      df = df.replace(' ?', np.nan)
-      
-      # one hot encode categorical columns
-      cat_cols = df.select_dtypes(include='object').columns.drop('income')
-      df = pd.get_dummies(df, columns=cat_cols)
-      
-      # drop fnlwt for now. 
-      # TODO: 
-      # try using fnlwgt as a sample_weight in model's fit()
-      # try using fnlwgt as a logged feature, instead of being standard scaled
-      df = df.drop('fnlwgt', axis=1) 
-      
-      # create new column for net capital
-      df["net_capital"] = df["capital_gain"] - df["capital_loss"]
-      
-      # create new column for is senior
-      df["is_senior"] = (df["age"] >= 65).astype(int)
-      
-      df['income_is_greater_than_50k'] = (df['income'].str.strip() == '>50K').astype(int)
-      df = df.drop('income', axis=1)
-      
-      # split into X and Y. Remember to do this before scaling, since we dont need to scale Y.    
-      X = df.drop('income_is_greater_than_50k', axis=1)
-      y = df['income_is_greater_than_50k']
-      
-      # scale numerical values
-      scaler = StandardScaler()
-      X_scaled = scaler.fit_transform(X)
-      
-      print(X.head()) 
-      print(y.head()) 
-      
-      self.df = df
-      self.X_scaled = X_scaled
-      self.y = y
-      self.next(self.get_train_test_split)
-      
-    @step
-    def get_train_test_split(self):
-      """
-      Get train test split.
-      """
-      y = self.y
-      X_scaled = self.X_scaled
+        print("Starting run at", datetime.utcnow())
+        self.next(self.load_data)
 
-      X_trainval, X_test, y_trainval, y_test = train_test_split(X_scaled, y, test_size=self.TEST_SPLIT, random_state=42)
-      X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=0.25, random_state=42)
-      
-      self.X_train = X_train.tolist()
-      self.X_val = X_val.tolist()
-      self.X_test = X_test.tolist()
-      self.y_train = y_train.tolist()
-      self.y_val = y_val.tolist()
-      self.y_test = y_test.tolist()  
-      
-      self.next(self.set_hyperparameter_grid_and_start_training)
-    
     @step
-    def set_hyperparameter_grid_and_start_training(self):
-      self.param_grid = [
-        {"C": 0.1, "penalty": "l2", "solver": "liblinear", "fit_intercept": True},
-        {"C": 1.0, "penalty": "l2", "solver": "liblinear", "fit_intercept": True},
-        {"C": 1.0, "penalty": "l1", "solver": "liblinear", "fit_intercept": False},
-        {"C": 10.0, "penalty": "l2", "solver": "lbfgs", "fit_intercept": True},
-        {"C": 1e-10, "penalty": "l2", "solver": "lbfgs", "fit_intercept": False},
-      ]
-      self.next(self.train_model, foreach="param_grid")
-    
+    def load_data(self):
+        df = pd.read_csv(StringIO(self.DATA_FILE), header=None)
+        df.columns = COLUMN_NAMES
+        df = df.replace(' ?', np.nan).dropna()
+        self.df = df.sample(frac=0.3, random_state=42).reset_index(drop=True)
+        self.next(self.prepare_data)
+
+    @step
+    def prepare_data(self):
+        df = self.df.copy()
+        self.sample_weights = df['fnlwgt'].values
+        df = df.drop(columns='fnlwgt')
+        df["net_capital"] = df["capital_gain"] - df["capital_loss"]
+        df["is_senior"] = (df["age"] >= 65).astype(int)
+
+        cat_cols = df.select_dtypes(include='object').columns.drop(TARGET_VALUE)
+        df = pd.get_dummies(df, columns=cat_cols)
+        y = LabelEncoder().fit_transform(df[TARGET_VALUE])
+        X = df.drop(columns=TARGET_VALUE)
+        X_scaled = StandardScaler().fit_transform(X)
+
+        self.X_scaled, self.y = X_scaled, y
+        self.next(self.split_data)
+
+    @step
+    def split_data(self):
+        X_temp, self.X_test, y_temp, self.y_test, w_temp, self.w_test = train_test_split(
+            self.X_scaled, self.y, self.sample_weights, test_size=self.TEST_SPLIT, random_state=42)
+        self.X_train, self.X_val, self.y_train, self.y_val, self.w_train, self.w_val = train_test_split(
+            X_temp, y_temp, w_temp, test_size=self.TEST_SPLIT, random_state=42)
+
+        for attr in ["X_train", "X_val", "X_test"]:
+            setattr(self, attr, getattr(self, attr).tolist())
+        for attr in ["y_train", "y_val", "y_test", "w_train", "w_val", "w_test"]:
+            setattr(self, attr, getattr(self, attr).tolist())
+
+        self.param_grid = PARAMETER_GRID
+        self.next(self.train_model, foreach='param_grid')
+
     @step
     def train_model(self):
-      
-      from sklearn import linear_model
-      from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score  
-      
-      params = self.input
-      self.hyperparams = params
+        params = self.input
+        model = linear_model.LogisticRegression(**params, multi_class='multinomial', random_state=42)
+        model.fit(self.X_train, self.y_train, sample_weight=self.w_train)
+        y_pred = model.predict(self.X_val)
 
-      model = linear_model.LogisticRegression(**params)
-      model.fit(self.X_train, self.y_train)
-      y_pred = model.predict(self.X_val)
-      
-      acc = accuracy_score(self.y_val, y_pred)
-      self.val_accuracy = acc
-      self.ps = precision_score(self.y_val, y_pred, zero_division=0)
-      self.rs = recall_score(self.y_val, y_pred)
-      self.f1 = f1_score(self.y_val, y_pred)
-      self.roc = roc_auc_score(self.y_val, y_pred)
-      
-      metrics = {
-        'accuracy': self.val_accuracy,
-        'precision_score': self.ps,
-        'recall_score': self.rs,
-        'f1_score': self.f1,
-        'auc_roc': self.roc
-      }
-      
-      exp = Experiment(project_name=os.environ['MY_PROJECT_NAME'],
-                    auto_param_logging=False)
-      exp.log_parameters(params)
-      exp.log_metrics(metrics)  
-      exp.end()
-      
-      self.next(self.join_models)
-      
+        avg = 'macro' if type_of_target(self.y_val) != 'binary' else 'binary'
+        metrics = {
+            'accuracy': accuracy_score(self.y_val, y_pred, sample_weight=self.w_val),
+            'precision': precision_score(self.y_val, y_pred, average=avg, zero_division=0, sample_weight=self.w_val),
+            'recall': recall_score(self.y_val, y_pred, average=avg, sample_weight=self.w_val),
+            'f1': f1_score(self.y_val, y_pred, average=avg, sample_weight=self.w_val)
+        }
+
+        exp = Experiment(project_name=os.environ['MY_PROJECT_NAME'], auto_param_logging=False)
+        exp.log_parameters(params)
+        exp.log_metrics(metrics)
+        exp.end()
+
+        self.metrics = metrics
+        self.model_params = params
+        self.next(self.select_best_model)
+
     @step
-    def join_models(self, inputs):
-        # Pick the best model based on validation accuracy
-        best = max(inputs, key=lambda x: x.val_accuracy)
-        print(best.input)
-        print(best.val_accuracy)
+    def select_best_model(self, inputs):
+        best = max(inputs, key=lambda x: x.metrics['accuracy'])
+        self.best_params = best.model_params
+        self.X_train = best.X_train + best.X_val
+        self.y_train = best.y_train + best.y_val
+        self.w_train = best.w_train + best.w_val
+        self.X_test = best.X_test
+        self.y_test = best.y_test
+        self.w_test = best.w_test
+        self.next(self.final_eval)
+
+    @step
+    def final_eval(self):
+        model = linear_model.LogisticRegression(**self.best_params, multi_class='multinomial', random_state=42)
+        model.fit(self.X_train, self.y_train, sample_weight=self.w_train)
+        y_pred = model.predict(self.X_test)
+
+        avg = 'macro' if type_of_target(self.y_test) != 'binary' else 'binary'
+        final_metrics = {
+            'accuracy': accuracy_score(self.y_test, y_pred, sample_weight=self.w_test),
+            'precision': precision_score(self.y_test, y_pred, average=avg, zero_division=0, sample_weight=self.w_test),
+            'recall': recall_score(self.y_test, y_pred, average=avg, sample_weight=self.w_test),
+            'f1': f1_score(self.y_test, y_pred, average=avg, sample_weight=self.w_test)
+        }
+
+        exp = Experiment(project_name=os.environ['MY_PROJECT_NAME'], auto_param_logging=False, 
+                         experiment_name=f"best-{uuid.uuid4().hex[:6]}")
+        exp.log_parameters(self.best_params)
+        exp.log_metrics(final_metrics)
+        exp.end()
+
+        print("Final test metrics:", final_metrics)
         self.next(self.end)
 
     @step
     def end(self):
-      # all done, just print goodbye
-      print("All done at {}!\n See you, space cowboys!".format(datetime.utcnow()))
-      
+        print("All done at", datetime.utcnow())
+
 if __name__ == '__main__':
     LogisticRegressionFlow()
